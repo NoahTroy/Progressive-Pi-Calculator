081a41fe654f8706baabcb608b9cf1664d324ba2
Noah Troy <NoahTroy@users.noreply.github.com>
2016-03-14 10:23:56 -0400

    Update
    Add basic GUI starting format

--- GUI Script WIP.py
+++ GUI Script WIP.py
@@ -1,1 +1,8 @@
+#Import toolkit interface as our GUI:
+from tkinter import *
+#Create interface and give it a title:
+window = Tk()
+window.title('Progressive Pi Calculator')
 
+#Dot-Suffix "mainloop" to the window, to capture window modifications (such as when the window is closed):
+window.mainloop()
